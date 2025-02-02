926dd8eefe8f7d2c2bb653cc55c88ce7adec77e9
Sasha Okoronko <hvorost22@mail.ru>
2025-02-02 16:39:02 +0300

    sdfgsgsdfs

--- src/js/modules/burger-menu.js
+++ src/js/modules/burger-menu.js
@@ -1,10 +1,18 @@
 const burgerMenu = () => {
     const burgerButton = document.querySelector('.burger-menu');
     const headerOverlay = document.querySelector('.header__overlay');
+    const links = document.querySelectorAll('.header__menu-link');
 
     burgerButton.addEventListener('click', () => {
         headerOverlay.classList.toggle('is-active');
+        document.documentElement.classList.toggle('no-scroll');
     });
+
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            headerOverlay.classList.remove('is-active');
+        })
+    })
 }
 
 export default burgerMenu;
