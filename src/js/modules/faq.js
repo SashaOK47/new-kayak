4d0fca267696c986f6e3381d8b455393f800f19c
Sasha Okoronko <hvorost22@mail.ru>
2025-02-02 17:53:09 +0300

    sderweqw

---
+++ src/js/modules/faq.js
@@ -0,0 +1,18 @@
+const faq = () => {
+    const faqItem = document.querySelectorAll(".faq__item");
+    faqItem.forEach((el) => {
+        el.addEventListener("click", (e) => {
+            e.preventDefault();
+            const currentItem = e.target.closest(".faq__item");
+            const content = e.target.nextElementSibling;
+            currentItem.classList.toggle("active");
+            if (currentItem.classList.contains("active")) {
+                content.style.maxHeight = content.scrollHeight + "px";
+            } else {
+                content.style.maxHeight = null;
+            }
+        });
+    });
+}
+
+export default faq;
