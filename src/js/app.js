919525e4a7b9a5dd1e12d9a78c780e99d3edf784
Sasha Okoronko <hvorost22@mail.ru>
2025-02-16 01:09:51 +0300

    hfghc

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,15 @@
-import { initApp } from "./init.js";
+import { switchTheme } from "./components/switch-theme.js";
+import { burgerMenu } from "./components/burger-menu.js";
+import { faq } from "./components/faq.js";
+import modalRules from "./components/modal-rules.js";
+import modalPrivacy from "./components/modal-privacy.js";
+import modalFeedback from "./components/modal-feedback.js";
+import { initForms } from "./components/inits/init-forms.js";
 
-window.addEventListener("DOMContentLoaded", () => {
-    initApp();
-});
-
-
+initForms();
+switchTheme();
+burgerMenu();
+faq();
+modalRules();
+modalPrivacy();
+modalFeedback();
