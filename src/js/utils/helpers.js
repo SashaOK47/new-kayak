f1c6c5f5af8a5401fc22f97b9eb870377aebb486
Sasha Okoronko <hvorost22@mail.ru>
2025-03-01 15:40:07 +0300

    sdfsdfs

--- src/js/utils/helpers.js
+++ src/js/utils/helpers.js
@@ -1,28 +1,7 @@
-import Swal from 'sweetalert2';
-
 export const successMessage = () => {
-    Swal.fire({
-        title: "Ваше сообщение отправлено!",
-        text: "Наш менеджер свяжется с вами в ближайшее время",
-        icon: "success",
-        confirmButtonColor: "#FB6406",
-        timer: 4000
-    });
-
-    // Swal.fire({
-    //     position: "top-end",
-    //     icon: "success",
-    //     title: "Your work has been saved",
-    //     showConfirmButton: false,
-    //     timer: 1500
-    // });
+    alert('Ваше сообщение отправлено!')
 }
 
 export const errorMessage = () => {
-    Swal.fire({
-        title: "Ошибка отправки данных!",
-        text: "Сообщение не отправлено. Попробуйте ещё раз!",
-        icon: "error",
-        timer: 4000
-    });
+    alert('Ошибка отправки данных!')
 }
